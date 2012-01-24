fd9c1874d1477132982e6ee248d8ddc90b2e5939
CI on MB02 <no_email_setup@metageek.net>
2012-01-24 14:03:57 -0700

    Version=2.0.7.1366

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -31,5 +31,5 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Revision and Build Numbers
 // by using the '*' as shown below:
-[assembly: AssemblyFileVersion("1.1.0.1354")]
-[assembly: AssemblyVersion("1.1.0.1354")]
+[assembly: AssemblyFileVersion("1.1.0.1366")]
+[assembly: AssemblyVersion("1.1.0.1366")]
